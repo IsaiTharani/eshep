a54b9ddb62d19f97255c25cef54e88ed4cadfab5
Thara-tea <isaidoss07@gmail.com>
2024-07-23 18:31:42 +0530

    eshep

---
+++ shep/src/main/java/com/eshop/shep/ShepApplication.java
@@ -0,0 +1,13 @@
+package com.eshop.shep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShepApplication.class, args);
+	}
+
+}
---
+++ shep/src/test/java/com/eshop/shep/ShepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshop.shep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
