5e02f8981f6bcd636c8500167402c166a8afe0e9
Thara-tea <isaidoss07@gmail.com>
2024-07-24 09:18:43 +0530

    Initial commit

---
+++ src/test/java/com/eshep/shep/ShepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshep.shep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
